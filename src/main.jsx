49270c1456e77ca298eefc81f8a5696caf65dd67
Andrew Kim <andrew.kim@c3.ai>
2022-06-13 15:34:35 -0700

    homepage package.json

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
+import starfall from './starfall'
+import threemin from './threemin'
+import capture from './capture'
 
 ReactDOM.render(
   <React.StrictMode>
