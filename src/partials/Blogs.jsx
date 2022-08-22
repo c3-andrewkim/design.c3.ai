d448c45aee46b10a5e24dd6f3e9926b82a7cd254
Andrew Kim <andrew.kim@c3.ai>
2022-08-22 15:47:21 -0700

    medium link to dev portal link

--- src/partials/Blogs.jsx
+++ src/partials/Blogs.jsx
@@ -35,7 +35,7 @@ function Blogs() {
               {/* 1st article */}
               <article className="flex flex-col h-full" data-aos="zoom-y-out">
                 <header>
-                  <a target="_blank" rel="noopener noreferrer" href="https://medium.com/c3-ai-design/intro-to-c3-ai-design-blog-series-fff7fd0c78b6" className="block mb-6">
+                  <a target="_blank" rel="noopener noreferrer" href="https://developer.c3.ai/blog/design-blog-series/introducing-c3-ai-design-blog-series" className="block mb-6">
                     <figure className="relative h-0 pb-9/16 overflow-hidden translate-z-0">
                       <img className="aspect-video absolute w-full h-full transform hover:-translate-y-1 transition duration-700 ease-out" src={introducing} width="352" height="198" alt="News 01" />
                     </figure>
@@ -52,7 +52,7 @@ function Blogs() {
                   </div> */}
                   <span className='font-bold dark-secondary-font-color text-base'>February 3, 2022</span>
                   <h3 className="text-lg font-bold leading-snug tracking-tight">
-                    <a target="_blank" rel="noopener noreferrer" href="https://medium.com/c3-ai-design/intro-to-c3-ai-design-blog-series-fff7fd0c78b6" className="hover:underline text-white">Intro to AI design blog series</a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://developer.c3.ai/blog/design-blog-series/introducing-c3-ai-design-blog-series" className="hover:underline text-white">Intro to AI design blog series</a>
                   </h3>
                 </header>
                 {/* <footer className="text-sm flex items-center mt-4">
@@ -72,14 +72,14 @@ function Blogs() {
               {/* 3rd article */}
               {/* <article className="flex flex-col h-full" data-aos="zoom-y-out">
                 <header>
-                  <a target="_blank" rel="noopener noreferrer" href="https://medium.com/c3-ai-design/intro-to-c3-ai-design-blog-series-fff7fd0c78b6" className="block mb-6">
+                  <a target="_blank" rel="noopener noreferrer" href="https://developer.c3.ai/blog/design-blog-series/introducing-c3-ai-design-blog-series" className="block mb-6">
                     <figure className="relative h-0 pb-9/16 overflow-hidden translate-z-0">
                       <img className="absolute inset-0 w-full h-full object-cover transform scale-105 hover:-translate-y-1 transition duration-700 ease-out" src={blogseries} width="352" height="198" alt="News 01" />
                     </figure>
                   </a>
                   <span className='font-bold dark-secondary-font-color text-xs'>Feb 3, 2022</span>
                   <h3 className="text-xl font-bold leading-snug tracking-tight">
-                    <a target="_blank" rel="noopener noreferrer" href="https://medium.com/c3-ai-design/intro-to-c3-ai-design-blog-series-fff7fd0c78b6" className="hover:underline text-white">Intro to AI design blog series</a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://developer.c3.ai/blog/design-blog-series/introducing-c3-ai-design-blog-series" className="hover:underline text-white">Intro to AI design blog series</a>
                   </h3>
                 </header>
               </article> */}
