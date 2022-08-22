72df705cc5eba3ac522cd5d1a62652872cb253e6
Andrew Kim <andrew.kim@c3.ai>
2022-08-22 14:58:06 -0700

    new blog post

--- src/partials/Blogs.jsx
+++ src/partials/Blogs.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import blogseries from '../images/blog-series.webp';
+import introducing from '../images/introducing.webp';
+import howto from '../images/howto.webp';
 
 function Blogs() {
   return (
-    <section id="blogs" className='gray-95 hidden'>
+    <section id="blogs" className='blogs gray-95 relative'>
       <div className="max-w-6xl mx-auto px-8 md:px-4">
         <div className="py-12 md:py-20">
 
@@ -20,7 +21,7 @@ function Blogs() {
                 <header>
                   <a target="_blank" rel="noopener noreferrer" href="https://medium.com/c3-ai-design/intro-to-c3-ai-design-blog-series-fff7fd0c78b6" className="block mb-6">
                     <figure className="relative h-0 pb-9/16 overflow-hidden translate-z-0">
-                      <img className="absolute inset-0 w-full h-full object-cover transform scale-105 hover:-translate-y-1 transition duration-700 ease-out" src={blogseries} width="352" height="198" alt="News 01" />
+                      <img className="aspect-video absolute w-full h-full transform hover:-translate-y-1 transition duration-700 ease-out" src={introducing} width="352" height="198" alt="News 01" />
                     </figure>
                   </a>
                   {/* <div className="mb-3">
@@ -33,24 +34,54 @@ function Blogs() {
                       </li>
                     </ul>
                   </div> */}
+                  <span className='font-bold dark-secondary-font-color text-xs'>February 3, 2022</span>
+                  <h3 className="text-xl font-bold leading-snug tracking-tight">
+                    <a target="_blank" rel="noopener noreferrer" href="https://medium.com/c3-ai-design/intro-to-c3-ai-design-blog-series-fff7fd0c78b6" className="hover:underline text-white">Intro to AI design blog series</a>
+                  </h3>
+                </header>
+                {/* <footer className="text-sm flex items-center mt-4">
+                  <div className="flex shrink-0 mr-3">
+                    <a className="relative" href="#0">
+                      <span className="absolute inset-0 -m-px" aria-hidden="true"><span className="absolute inset-0 -m-px bg-white rounded-full"></span></span>
+                      <img className="relative rounded-full" src={NewsAuthor01} width="32" height="32" alt="Author 01" />
+                    </a>
+                  </div>
+                  <div>
+                    <span className="text-gray-600">By </span>
+                    <a className="font-medium hover:underline dark-secondary-font-color" href="#0">Artificial Intelligence</a>
+                  </div>
+                </footer> */}
+              </article>
+
+              {/* 2nd article */}
+              <article className="flex flex-col h-full" data-aos="zoom-y-out">
+                <header>
+                  <a target="_blank" rel="noopener noreferrer" href="https://developer.c3.ai/blog/design-blog-series/how-design-platforms-improve-engagement-ai-beginners" className="block mb-6">
+                    <figure className="relative h-0 pb-9/16 overflow-hidden translate-z-0">
+                      <img className="aspect-video absolute w-full h-full transform hover:-translate-y-1 transition duration-700 ease-out" src={howto} width="352" height="198" alt="How to article thumbnail" />
+                    </figure>
+                  </a>
+                  <span className='font-bold dark-secondary-font-color text-xs'>August 8, 2022</span>
+                  <h3 className="text-xl font-bold leading-snug tracking-tight">
+                    <a target="_blank" rel="noopener noreferrer" href="https://developer.c3.ai/blog/design-blog-series/how-design-platforms-improve-engagement-ai-beginners" className="hover:underline text-white">How to Design Platforms that Improve Engagement with AI Beginners</a>
+                  </h3>
+                </header>
+              </article>
+
+              {/* 3rd article */}
+              {/* <article className="flex flex-col h-full" data-aos="zoom-y-out">
+                <header>
+                  <a target="_blank" rel="noopener noreferrer" href="https://medium.com/c3-ai-design/intro-to-c3-ai-design-blog-series-fff7fd0c78b6" className="block mb-6">
+                    <figure className="relative h-0 pb-9/16 overflow-hidden translate-z-0">
+                      <img className="absolute inset-0 w-full h-full object-cover transform scale-105 hover:-translate-y-1 transition duration-700 ease-out" src={blogseries} width="352" height="198" alt="News 01" />
+                    </figure>
+                  </a>
                   <span className='font-bold dark-secondary-font-color text-xs'>Feb 3, 2022</span>
                   <h3 className="text-xl font-bold leading-snug tracking-tight">
                     <a target="_blank" rel="noopener noreferrer" href="https://medium.com/c3-ai-design/intro-to-c3-ai-design-blog-series-fff7fd0c78b6" className="hover:underline text-white">Intro to AI design blog series</a>
                   </h3>
                 </header>
-                <footer className="text-sm flex items-center mt-4">
-                  {/* <div className="flex shrink-0 mr-3">
-                    <a className="relative" href="#0">
-                      <span className="absolute inset-0 -m-px" aria-hidden="true"><span className="absolute inset-0 -m-px bg-white rounded-full"></span></span>
-                      <img className="relative rounded-full" src={NewsAuthor01} width="32" height="32" alt="Author 01" />
-                    </a>
-                  </div> */}
-                  <div>
-                    {/* <span className="text-gray-600">By </span> */}
-                    <a className="font-medium hover:underline dark-secondary-font-color" href="#0">Artificial Intelligence</a>
-                  </div>
-                </footer>
-              </article>
+              </article> */}
 
             </div>
           </div>
